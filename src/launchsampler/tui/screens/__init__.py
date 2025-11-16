f68019a91f6625505d9a689781b8d66f7f4e4f1a
Adrien <afauconnet@gmail.com>
2025-11-16 12:40:36 +0100

    remove dead code

--- src/launchsampler/tui/screens/__init__.py
+++ src/launchsampler/tui/screens/__init__.py
@@ -6,17 +6,10 @@ from .directory_browser import DirectoryBrowserScreen
 from .set_file_browser import SetFileBrowserScreen
 from .save_set_browser import SaveSetBrowserScreen
 
-# Legacy screens - deprecated, use browser-based screens instead
-from .save_set import SaveSetScreen
-from .load_set import LoadSetScreen
-
 __all__ = [
     "BaseBrowserScreen",
     "FileBrowserScreen",
     "DirectoryBrowserScreen",
     "SetFileBrowserScreen",
     "SaveSetBrowserScreen",
-    # Legacy - deprecated
-    "SaveSetScreen",
-    "LoadSetScreen",
 ]
